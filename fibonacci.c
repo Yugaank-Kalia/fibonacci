dc3ceaa5d1f43bbb6a986b6e0b747837d5cb8b44
Yugaank Kalia <yugaank@outlook.com>
2018-04-03 18:24:28 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
-int main()
-print f
-return
+
+int fib(n){
+  if(n==0) return 0;
+  else if(n==1) return 1;
+  else return fib(n-2)+fib(n-1);
+
+}
+
+int main(){
+  printf("%d",fib(5));
+ return 0;
+
+}
